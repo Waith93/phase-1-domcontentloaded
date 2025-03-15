539b18f470fc355412aa11dc45c7a0cfc4471b01
Waithera <stacywaithera@DESKTOP-E0D09HN>
2025-03-15 17:48:18 +0300

    dom content

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+    
+    let paragraph = document.getElementById("text");
+    
+    paragraph.textContent = "This is really cool!";
+});
